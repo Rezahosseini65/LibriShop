3a2d526bbe2c056f1f545f5e42b2bcedaeaf35d3
reza <hosseini.reza65@gmail.com>
2024-06-21 15:03:02 +0330

    Initial project.

--- src/librishop/asgi.py
+++ src/librishop/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'librishop.envs.develoment')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'librishop.envs.development')
 
 application = get_asgi_application()
--- src/librishop/envs/development.py
+++ src/librishop/envs/development.py
@@ -1,1 +1,27 @@
 from .common import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'daphne',
+    'drf_spectacular',
+    'debug_toolbar',
+]+INSTALLED_APPS
+
+INTERNAL_IPS = [
+    "0.0.0.0",
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'librishop',
+        'USER': 'librishop',
+        'PASSWORD': '123@456',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+
